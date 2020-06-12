ee090d8ec6e73046c15c1218a92c0cd30243a0df
dsb <dongsubak@users.noreply.github.com>
2020-06-12 16:57:14 +0900

    Update articles.js ...state.articles to ...state , articles to article on VIEW_ARTICLE

--- src/redux/reducers/articles.js
+++ src/redux/reducers/articles.js
@@ -6,13 +6,13 @@ export default (state=initialState, action) => {
   switch (action.type) {
     case 'LOAD_ARTICLES' :
     return {
-      ...state.article,
+      ...state,
       articles: action.articles
     }
     case 'VIEW_ARTICLE' :
     return {
-      ...state.article,
-      articles: action.articles
+      ...state,
+      article: action.article
     }
     case 'CLAP_ARTICLE':
     let article = Object.assign({}, state.article)
